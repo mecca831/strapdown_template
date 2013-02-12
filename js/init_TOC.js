bc52d8c96cd98041affe3ef8d1971470a181a987
zipizap <zipizap123@gmail.com>
2013-02-12 23:47:12 +0100

    nip

--- js/init_TOC.js
+++ js/init_TOC.js
@@ -12,57 +12,56 @@ $('#toc').toc({
 // There you go, there is a reason for it, and I do admit that is far from perfect - so if you ever read this and know a better
 // solution, please fork + improve + pull and send me a message, I'll be glad to see it looking better :)
 
-$('head').append('<link href="https://github.com/zipizap/strapdown_template/raw/master/css/TOC.css" rel="stylesheet">');
-//$('head').prepend('' +
-//    '<style media="screen" type="text/css">  \n' +
-//    '#toc {  \n' +
-//    '    top: auto;  \n' +
-//    '    left: 0px;  \n' +
-//    '    height: 100%;  \n' +
-//    '    position: static;  \n' +
-//    '    background: #333;  \n' +
-//    '    box-shadow: inset -5px 0 5px 0px #000;  \n' +
-//    '    width: auto;  \n' +
-//    '    padding-top: 20px;  \n' +
-//    '    color: #fff;  \n' +
-//    '}  \n' +
-//    '  \n' +
-//    '#toc ul {  \n' +
-//    '    margin: 0;  \n' +
-//    '    padding: 0;  \n' +
-//    '    list-style: none;  \n' +
-//    '}  \n' +
-//    '  \n' +
-//    '#toc li {  \n' +
-//    '    padding: 5px 10px;  \n' +
-//    '}  \n' +
-//    '  \n' +
-//    '#toc a {  \n' +
-//    '    color: #fff;  \n' +
-//    '    text-decoration: none;  \n' +
-//    '    display: block;  \n' +
-//    '}  \n' +
-//    '#toc .toc-h1 {  \n' +
-//    '    padding-left: 5px;  \n' +
-//    '}  \n' +
-//    '  \n' +
-//    '#toc .toc-h2 {  \n' +
-//    '    padding-left: 10px;  \n' +
-//    '}  \n' +
-//    '  \n' +
-//    '#toc .toc-h3 {  \n' +
-//    '    padding-left: 20px;  \n' +
-//    '}  \n' +
-//    '#toc .toc-h4 {  \n' +
-//    '    padding-left: 30px;  \n' +
-//    '}  \n' +
-//    '#toc .toc-h5 {  \n' +
-//    '    padding-left: 40px;  \n' +
-//    '}  \n' +
-//    '  \n' +
-//    '#toc .toc-active {  \n' +
-//    '    background: #336699;  \n' +
-//    '    box-shadow: inset -5px 0px 10px -5px #000;  \n' +
-//    '}  \n' +
-//    '</style>  \n' +
-//    '');
+$('head').prepend('' +
+    '<style media="screen" type="text/css">  \n' +
+    '#toc {  \n' +
+    '    top: auto;  \n' +
+    '    left: 0px;  \n' +
+    '    height: 100%;  \n' +
+    '    position: static;  \n' +
+    '    background: #333;  \n' +
+    '    box-shadow: inset -5px 0 5px 0px #000;  \n' +
+    '    width: auto;  \n' +
+    '    padding-top: 20px;  \n' +
+    '    color: #fff;  \n' +
+    '}  \n' +
+    '  \n' +
+    '#toc ul {  \n' +
+    '    margin: 0;  \n' +
+    '    padding: 0;  \n' +
+    '    list-style: none;  \n' +
+    '}  \n' +
+    '  \n' +
+    '#toc li {  \n' +
+    '    padding: 5px 10px;  \n' +
+    '}  \n' +
+    '  \n' +
+    '#toc a {  \n' +
+    '    color: #fff;  \n' +
+    '    text-decoration: none;  \n' +
+    '    display: block;  \n' +
+    '}  \n' +
+    '#toc .toc-h1 {  \n' +
+    '    padding-left: 5px;  \n' +
+    '}  \n' +
+    '  \n' +
+    '#toc .toc-h2 {  \n' +
+    '    padding-left: 10px;  \n' +
+    '}  \n' +
+    '  \n' +
+    '#toc .toc-h3 {  \n' +
+    '    padding-left: 20px;  \n' +
+    '}  \n' +
+    '#toc .toc-h4 {  \n' +
+    '    padding-left: 30px;  \n' +
+    '}  \n' +
+    '#toc .toc-h5 {  \n' +
+    '    padding-left: 40px;  \n' +
+    '}  \n' +
+    '  \n' +
+    '#toc .toc-active {  \n' +
+    '    background: #336699;  \n' +
+    '    box-shadow: inset -5px 0px 10px -5px #000;  \n' +
+    '}  \n' +
+    '</style>  \n' +
+    '');
